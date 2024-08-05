a41724a1c47f60897df7592afdeba518ff97b709
Ahmed El Gendy <125159880+Ahmed-El-Gendy@users.noreply.github.com>
2024-08-05 08:18:10 +0000

    pycodestule updated

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
+"""
+pascal traingle
+"""
+
 
 def pascal_triangle(n):
+    """
+    return 2d list of pascal traingle
+    """
     arr = []
+    if n <= 0:
+        return arr
     for i in range(n):
         col = [1] * (i + 1)
         for j in range(1, i):
-            col[j]=(arr[i-1][j-1]+arr[i-1][j])
+            col[j] = (arr[i - 1][j - 1] + arr[i - 1][j])
         arr.append(col)
     return arr
 
-if __name__ == '__main__':
-    print(pascal_triangle(5))
 
+if __name__ == '__main__':
+    print(pascal_triangle(0))
