91c58cfe074f22cd5d5ede90d80c55830dd50f41
Ahmed El Gendy <125159880+Ahmed-El-Gendy@users.noreply.github.com>
2024-08-13 08:34:21 +0000

    task0

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,14 +3,27 @@
 Check if I can open all boxes with DFS
 """
 
+
 visit = set()
 sz = 0
+
+'''
+DfS function
+'''
+
 
 def dfs(node, boxes):
     visit.add(node)
     for i in boxes[node]:
         if i not in visit and i < sz:
             dfs(i, boxes)
+
+
+
+'''
+The main function
+'''
+
 
 def canUnlockAll(boxes):
     global sz
