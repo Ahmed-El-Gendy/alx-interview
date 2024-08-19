3eeae600e4a203fb89a5e9cc9f4e202091d9ed55
Ahmed El Gendy <125159880+Ahmed-El-Gendy@users.noreply.github.com>
2024-08-19 06:34:15 +0000

    minoperations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Min steps to reach
+"""
+
+
+def minOperations(n):
+    """ Minimum operations to reach n """
+    if n <= 1:
+        return 0
+
+    operations = 0
+    factor = 2
+
+    while n > 1:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+        return operations
