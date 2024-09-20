0f70abffdb838da0a3b8c873f6e83bec2396f77e
Ahmed El Gendy <125159880+Ahmed-El-Gendy@users.noreply.github.com>
2024-09-20 16:58:35 +0000

    task0

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,28 +2,42 @@
 
 const request = require('request');
 
-function printCharacters(characters) {
-    characters.forEach((url) => {
+
+function fetch(url) {
+    return new Promise((resolve, reject) => {
         request(url, (error, response, body) => {
-            if (!error) {
-                const character = JSON.parse(body);
-                console.log(character.name);
+            if (error) {
+                return reject(error);
             }
+            resolve(JSON.parse(body));
         });
     });
 }
 
-const movieId = process.argv[2];
-const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
 
-request(apiUrl, (error, response, body) => {
-    if (error) {
-        console.error(error);
-        return;
+async function printCharacters(characters) {
+    for (const url of characters) {
+        try {
+            const character = await fetch(url);
+            console.log(character.name);
+        } catch (error) {
+            console.error(error);
+        }
     }
+}
 
-    const filmData = JSON.parse(body);
-    const characters = filmData.characters;
 
-    printCharacters(characters);
-});
+async function main() {
+    const movieId = process.argv[2];
+    const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
+
+    try {
+        const filmData = await fetch(apiUrl);
+        const characters = filmData.characters;
+        await printCharacters(characters);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+main();
