c538dce6f498f0546026dbd414210c2e1f14424c
Ahmed El Gendy <125159880+Ahmed-El-Gendy@users.noreply.github.com>
2024-09-20 17:19:15 +0000

    task0

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,27 +3,27 @@
 const request = require('request');
 
 const fetchCharactersSequentially = (arr, idx) => {
-    if (idx === arr.length) {
-        return;
+  if (idx === arr.length) {
+    return;
+  }
+  request(arr[idx], (err, response, body) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log(JSON.parse(body).name);
+      fetchCharactersSequentially(arr, idx + 1);
     }
-    request(arr[idx], (err, response, body) => {
-        if (err) {
-            throw err;
-        } else {
-            console.log(JSON.parse(body).name);
-            fetchCharactersSequentially(arr, idx + 1);
-        }
-    });
+  });
 };
 
 request(
     `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
     (err, response, body) => {
-        if (err) {
-            throw err;
-        } else {
-            const characters = JSON.parse(body).characters;
-            fetchCharactersSequentially(characters, 0);
-        }
+      if (err) {
+        throw err;
+      } else {
+        const characters = JSON.parse(body).characters;
+        fetchCharactersSequentially(characters, 0);
+      }
     }
 );
