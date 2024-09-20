11e33b0813faf0bb5ebb6bc8bfe87f3216045791
Ahmed El Gendy <125159880+Ahmed-El-Gendy@users.noreply.github.com>
2024-09-20 17:00:08 +0000

    task0

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,43 +1,31 @@
 #!/usr/bin/node
 
-const request = require('request');
+const axios = require('axios');
 
-
-function fetch(url) {
-    return new Promise((resolve, reject) => {
-        request(url, (error, response, body) => {
-            if (error) {
-                return reject(error);
-            }
-            resolve(JSON.parse(body));
-        });
-    });
-}
-
-
-async function printCharacters(characters) {
-    for (const url of characters) {
-        try {
-            const character = await fetch(url);
-            console.log(character.name);
-        } catch (error) {
-            console.error(error);
-        }
-    }
-}
-
-
-async function main() {
-    const movieId = process.argv[2];
-    const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
-
+const fetchCharacters = async (urls) => {
     try {
-        const filmData = await fetch(apiUrl);
-        const characters = filmData.characters;
-        await printCharacters(characters);
+        for (let url of urls) {
+            const response = await axios.get(url);
+            console.log(response.data.name);
+        }
     } catch (error) {
         console.error(error);
     }
-}
+};
 
-main();
+const fetchFilmCharacters = async (filmId) => {
+    try {
+        const response = await axios.get(`https://swapi-api.hbtn.io/api/films/${filmId}`);
+        const characters = response.data.characters;
+        await fetchCharacters(characters);
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+const filmId = process.argv[2];
+if (filmId) {
+    fetchFilmCharacters(filmId);
+} else {
+    console.log('Please provide a film ID');
+}
