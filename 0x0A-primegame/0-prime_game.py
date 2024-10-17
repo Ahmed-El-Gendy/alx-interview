45ef24807525e4895545953864360e744be31c67
Ahmed El Gendy <125159880+Ahmed-El-Gendy@users.noreply.github.com>
2024-10-17 20:24:12 +0000

    fix code

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -11,17 +11,28 @@ def isWinner(x, nums):
     if not nums or x < 1:
         return None
 
-    n = max(nums)
-    sieve = [1] * (n + 1)
-    sieve[0] = sieve[1] = 0
-    for i in range(2, int(n ** 0.5) + 1):
-        if sieve[i]:
-            for j in range(i * i, n + 1, i):
-                sieve[j] = 0
+    Maria = Ben = 0
 
-    sieve = [i for i in range(n + 1) if sieve[i]]
-    wins = 0
-    for i in range(min(x, len(sieve))):
-        wins += sieve[i] in nums
+    for num in nums:
+        if num < 2:
+            Ben += 1
+            continue
 
-    return "Maria" if wins % 2 == 0 or wins == 0 else "Ben"
+        sieve = [True] * (num + 1)
+        sieve[0] = sieve[1] = False
+        for i in range(2, int(num ** 0.5) + 1):
+            if sieve[i]:
+                for j in range(i * i, num + 1, i):
+                    sieve[j] = False
+
+        primes = [i for i in range(2, num + 1) if sieve[i]]
+        if len(primes) % 2 == 0:
+            Ben += 1
+        else:
+            Maria += 1
+
+    if Maria > Ben:
+        return "Maria"
+    elif Ben > Maria:
+        return "Ben"
+    return None
